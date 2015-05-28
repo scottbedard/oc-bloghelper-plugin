cbd597b993af056c70a6329f654f92dc16782150
scottbedard <scottbedard90@gmail.com>
2015-05-27 20:01:29 -0400

    Adds layout and img tags

--- Plugin.php
+++ Plugin.php
@@ -32,11 +32,41 @@ class Plugin extends PluginBase
             $controller->addCss('/plugins/bedard/bloghelper/assets/css/style.css');
         });
 
-        // Listen for markdown parsing
+        // YouTube tags
         Event::listen('markdown.beforeParse', function($data) {
+
+            // [youtube]...[/youtube]
             $data->text = preg_replace(
                 '/\[youtube\](.{1,11})\[\/youtube\]/im',
                 '<div class="video-wrapper"><iframe type="text/html" src="http://www.youtube.com/embed/$1" frameborder="0"/></iframe></div>',
+                $data->text
+            );
+
+            // [img]...[/img]
+            $data->text = preg_replace(
+                '/\[img\]([\S\s]+?)\[\/img\]/im',
+                '<img src="$1">',
+                $data->text
+            );
+
+            // [half]...[/half]
+            $data->text = preg_replace(
+                '/\[half\]([\S\s]+?)\[\/half\]/im',
+                '<div class="md-layout half">$1</div>',
+                $data->text
+            );
+
+            // [third]...[/third]
+            $data->text = preg_replace(
+                '/\[third\]([\S\s]+?)\[\/third\]/im',
+                '<div class="md-layout third">$1</div>',
+                $data->text
+            );
+
+            // [fourth]...[/fourth] or [quarter]...[/quarter]
+            $data->text = preg_replace(
+                '/\[(fourth|quarter)\]([\S\s]+?)\[\/(fourth|quarter)\]/im',
+                '<div class="md-layout fourth">$1</div>',
                 $data->text
             );
         });
